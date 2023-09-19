db7bcd97543c83335f32912bd1ebc8b44592f0c0
Emily-Robeson <emily.robeson@gmail.com>
2023-09-19 16:38:30 +0100

    ver 1.5

--- resources/index.js
+++ resources/index.js
@@ -13,15 +13,54 @@ let movielist = [
         },
    ];
 
-function GetValue()
+function GetMovie()
 {
   var random = movielist[Math.floor(Math.random() * movielist.length)];
   document.getElementById("message").innerHTML=random.movieName;
 }
 
+
+
 let showlist = [
     {
         idShow: 1,
-        showName: ''
-    }
-]
+        showName: 'Taskmaster New Zealand, Service: Channel 4'
+        },
+    {
+        idShow: 2,
+        showName: 'Sex Education, Service: Netflix'
+        },
+    {
+        idShow: 3,
+        showName: 'The Orville, Service: Disney+'
+        },           
+];
+
+function GetShow()
+{
+  var random = showlist[Math.floor(Math.random() * showlist.length)];
+  document.getElementById("message").innerHTML=random.showName;
+}
+
+
+
+let youtubelist = [
+    {
+        idYoutube: 1,
+        youtubeName: 'Map Men'
+        },
+    {
+        idYoutube: 2,
+        youtubeName: 'Josh Strife'
+        },
+    {
+        idYoutube: 3,
+        youtubeName: 'Outside Xbox/Xtra'
+        },           
+];
+
+function GetYoutube()
+{
+  var random = youtubelist[Math.floor(Math.random() * youtubelist.length)];
+  document.getElementById("message").innerHTML=random.youtubeName;
+}
